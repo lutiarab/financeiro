c22b539370f2c12fdd245782ad933c7fdf424549
lutiarab <lutiara.barbosa@aluno.senai.br>
2024-09-13 10:09:34 -0300

    começando o projeto

---
+++ server.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv'); // Importar o pacote dontev para gerenciar variáveis de abiente
+
+// Configurar as variáveis de ambiente
+
+dotenv.config(); // Carrega as variáveis definidas no arquivo '.env' para process.env (processos)
+
+//Importar as bibliotecas
+
+const express = require('express'); // Importa a fremework express
