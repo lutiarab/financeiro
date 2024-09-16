a315ac8fae857794c776a4b7e053cdaaef7a0d19
lutiarab <lutiara.barbosa@aluno.senai.br>
2024-09-16 08:13:49 -0300

    alteração no serve e criamos o dados em uma pasta

--- server.js
+++ server.js
@@ -7,3 +7,17 @@ dotenv.config(); // Carrega as variáveis definidas no arquivo '.env' para proce
 //Importar as bibliotecas
 
 const express = require('express'); // Importa a fremework express
+const cors = require('cors'); // Importa o pacote cors para permitir requisições de diferentes origens
+
+const dados = require('./configurar/dados'); // Importa a conexão com o banco de dados
+
+// Inicializar nova aplicação Express
+
+const app = express(); // Inicializar uma nova aplicação Express
+
+// Configurar o CORS e o BADY-PARSE
+
+app.use(cors()); // Habilita o CORS para todas as rotas
+app.use(bodyparser.json()); // Configura o BODY-PARSER para analisar requisições JSON
+
+//Usar as rotas de TRANSAÇÕES e AUTENTICAÇÕES para as requisições
