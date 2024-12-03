77f1bab39c2a2f8c8dd9adc6c973673614bb63c3
lutiarab <lutiara.barbosa@aluno.senai.br>
2024-12-03 10:41:26 -0300

    novas alteraçoes

--- public/script.js
+++ public/script.js
@@ -15,3 +15,25 @@ signupLink.onclick = (()=>{
   signupBtn.click();
   return false;
 });
+
+/*............................................................................................................................................................ */
+
+
+ 
+ // Função para exibir o modal de login
+function mostrarModal() {
+  document.getElementById('login-modal').style.display = 'block';
+}
+
+// Função para fechar o modal de login
+function fecharModal() {
+  document.getElementById('login-modal').style.display = 'none';
+}
+
+// Fecha o modal ao clicar fora do conteúdo
+window.onclick = function(event) {
+  const modal = document.getElementById('login-modal');
+  if (event.target === modal) {
+      fecharModal();
+  }
+};
